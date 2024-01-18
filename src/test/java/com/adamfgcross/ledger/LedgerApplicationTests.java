86bdddb6cfa280b6f1f23c3828ffbbd449b38b08
Adam Cross <adamfgcross@gmail.com>
2024-01-18 13:13:20 -0500

    create ledger project

---
+++ src/test/java/com/adamfgcross/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
